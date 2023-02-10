8f3a7f59da4fcaf5b6ec1d85656d016c445d6cdc
Gustavo Henrique <ymooncake@gmail.com>
2023-02-10 10:32:25 -0300

    Raizes encontradas(10/02)

--- aula1/venv/project/main.py
+++ aula1/venv/project/main.py
@@ -1,1 +1,13 @@
-print('Jorge')
+# (X**2)-(2x-3)
+
+
+
+def Raizes():
+    delta = int(input('Digite o valor de X'))
+    delta = (-2**2) -(4*1*3)
+    x1 = (-2+delta**(1/2))/(2*1)
+    x2 = (-2-delta**(1/2))/(2*1)
+    return {x1,x2}
+
+if __name__ == '__main__': 
+   print(Raizes())
