6457ada1fbb47d79e031ae2f6fe6b39aa9e12f55
Gustavo Henrique <ymooncake@gmail.com>
2023-02-12 09:51:34 -0300

    Concertado(12/02)

--- aula1/venv/project/main.py
+++ aula1/venv/project/main.py
@@ -1,11 +1,17 @@
-# (X**2)-(2x-3)
+# f(x) = (X**2) - 2x-3 - Função usada como exemplo para o calculo das raizes
+# delta = (b**2) -4 * a * c -  Delta em função de X 
+# X1 = (-b + delta **(1/2))/2*a - Formula de achar as riazes da função
 
-def Raizes():
-    delta = int(input('Digite o valor de X'))
-    delta = (-2**2) -(4*1*3)
-    x1 = (-2+delta**(1/2))/(2*1)
-    x2 = (-2-delta**(1/2))/(2*1)
-    return {x1,x2}
 
-if __name__ == '__main__': 
-   print(Raizes())
+
+def rais(x):
+
+    Delta = (2**2) - 4 * 1 * (-3)
+
+    X1 =  (-2 + Delta **(1/2)) /2*1
+    X2 =  (-2 - Delta **(1/2)) /2*1
+
+    return X1,X2
+
+if __name__ == '__main__':
+    print(rais())
