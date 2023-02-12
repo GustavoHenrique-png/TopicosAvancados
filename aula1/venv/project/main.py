9977bdf7c2661a27d95b66bf8b7663c8e7ccb805
Gustavo Henrique <ymooncake@gmail.com>
2023-02-12 12:47:58 -0300

    Montando Graficos(12/02)

--- aula1/venv/project/main.py
+++ aula1/venv/project/main.py
@@ -1,10 +1,10 @@
+import matplotlib.pyplot as plt 
+
 # f(x) = (X**2) - 2x-3 - Função usada como exemplo para o calculo das raizes
 # delta = (b**2) -4 * a * c -  Delta em função de X 
 # X1 = (-b + delta **(1/2))/2*a - Formula de achar as riazes da função
 
-
-
-def rais(x):
+def trueRoots():
 
     Delta = (2**2) - 4 * 1 * (-3)
 
@@ -13,5 +13,14 @@ def rais(x):
 
     return X1,X2
 
+def generateGraphs(X1,X2):
+    XBar = [1,2,3,4,5,6,7,8,9,10]
+    YBar = [1,2,3,4,5,6,7,8,9,10]
+
+    plt.plot(X1,X2)
+
+
+
 if __name__ == '__main__':
-    print(rais())
+    print(trueRoots())
+    generateGraphs()
